8ecb0a92a565b62a58f7572a0e6f945c6787fe90
sustainablepace <christoph@baudson.de>
2014-10-18 12:01:50 +0200

    Experiments and subtitles

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,11 +1,20 @@
 <?php snippet('header') ?>
 
 <section class="content">
-
   <article>
-    <p class="date"><?php echo $page->date( 'F jS, Y' );?> </p>
-    <h1><?php echo html($page->title()) ?></h1>
-    <?php echo kirbytext($page->text()) ?>
+    <p class="date">
+      <?= $page->date( 'F jS, Y' ); ?>
+    </p>
+    <h1>
+      <?= html($page->title()) ?>
+      <?php if( $page->subtitle() ): ?>
+        <br>
+        <small>
+          (<?= html($page->subtitle()) ?>)
+        </small>
+      <?php endif; ?>
+    </h1>
+    <?= kirbytext( $page->text() ) ?>
     <?php snippet('disqus', array('disqus_shortname' => 'sustainablepace' )) ?>
     <a href="https://twitter.com/share" class="twitter-share-button" data-via="twitterapi" data-lang="en">Tweet</a>
 
