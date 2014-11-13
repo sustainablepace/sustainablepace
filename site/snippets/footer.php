552e4901d869e738f57be689d2fa3c5bd0243167
sustainablepace <christoph@baudson.de>
2014-11-13 09:09:38 +0100

    Google Analytics

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -12,10 +12,24 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-53518078-1', 'auto');
+  // Set to the same value as the web property used on the site
+  var gaProperty = 'UA-32666787-1';
+
+  ga('create', gaProperty, 'auto');
   ga('set', 'anonymizeIp', true);
   ga('send', 'pageview');
 
+  // Disable tracking if the opt-out cookie exists.
+  var disableStr = 'ga-disable-' + gaProperty;
+  if (document.cookie.indexOf(disableStr + '=true') > -1) {
+    window[disableStr] = true;
+  }
+
+  // Opt-out function
+  function gaOptout() {
+    document.cookie = disableStr + '=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/';
+    window[disableStr] = true;
+  }
 </script>
 </body>
 </html>
