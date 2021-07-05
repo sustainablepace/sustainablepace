20607952c2cd830d459f6b4d77c11a63e78c1328
christoph.baudson <christoph.baudson@rewe-digital.com>
2021-07-05 20:21:13 +0200

    zero waste

---
+++ site/templates/zerowaste.php
@@ -0,0 +1,29 @@
+<?php snippet('header') ?>
+
+<section class="content">
+  <article>
+  <h1><?= html($page->title()) ?></h1>
+  <p>
+    <?= kirbytext($page->text()) ?>
+  </p>
+    <dl>
+    <?php $siblings = $page->children()->visible()->flip(); ?>
+    <?php if( $siblings->count() > 0 ): ?>
+      <?php foreach( $siblings as $s ): ?>
+        <dt>
+          <small><?= $s->date( 'Y-m-d' ) ?></small>
+        </dt>
+        <dd>
+          &raquo;&nbsp;<a href="<?= $s->url() ?>"><?= $s->title() ?></a>
+        </dd>
+      <?php endforeach; ?>
+    <?php else: ?>
+      <p>
+        <em>I will add information on zero waste soon.</em>
+      <p>
+    <?php endif; ?>
+    </dl>
+  </article>
+</section>
+
+<?php snippet('footer') ?>
